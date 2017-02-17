6ce001ac945e0d39648aa3f0f8aa7c439dd365ce
Michael Gagnon <mikegagnon@gmail.com>
2017-02-16 18:27:11 -0700

    JSON parsing working

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,38 @@
 package controllers
 
 import play.api.mvc._
+import play.api.libs.json._
+
+// TODO: where should this case class go
+case class ContestRequest(petId1: String, petId2: String, contestType: String)
 
 object Application extends Controller {
 
+  implicit val contestRequestReads = Json.reads[ContestRequest]
+
   // TODO: rm along with view
   def index = Action {
+    Ok(views.html.main())
+  }
+
+  def contest = Action { request =>
+
+    /*
+    val parsed = request.body.asJson.map { jsValue =>
+      val petId1 = (jsValue \ "pet-id-1")
+      val petId2 = (jsValue \ "pet-id-2")
+      val contestType = (jsValue \ "contest-type")
+      (petId1, petId2, contestType)
+    }*/
+
+  val contestRequest: Option[JsResult[ContestRequest]] = request.body.asJson.map { jsValue : JsValue =>
+      Json.fromJson[ContestRequest](jsValue)
+  }
+
+
+
+    println(contestRequest)
+
     Ok(views.html.main())
   }
 
