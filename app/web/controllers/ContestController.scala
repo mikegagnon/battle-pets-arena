9445dd038b0a5e0fadb56ad2760907a73e4e85bd
Michael Gagnon <mikegagnon@gmail.com>
2017-02-22 11:55:57 -0700

    Clean ContestController

--- app/web/controllers/ContestController.scala
+++ app/web/controllers/ContestController.scala
@@ -17,20 +17,21 @@ import me.michaelgagnon.pets.web.ContestStatus
 
 case class ContestRequest(petId1: String, petId2: String, contestType: String)
 
+object ContestController {
+  implicit val contestRequestReads = Json.reads[ContestRequest]
+}
+
 @Singleton
 class ContestController @Inject()(config: Configuration) extends Controller {
+
+  import ContestController._
 
   // Note: The reason we specify parse.tolerantJson is to avoid Play automatically handling
   // the error case where application/json is missing from the header. We want to avoid this case
   // because Play returns an HTML error message, which is inconsistent with our JSON error messages
   def contest = Action(parse.tolerantJson) { request =>
 
-      implicit val contestRequestReads = Json.reads[ContestRequest]
-
-      // parse the request
       val result: JsResult[ContestRequest] = Json.fromJson[ContestRequest](request.body)
-
-      println(result)
 
       result match {
         case error: JsError => BadRequest("\"Your request is malformed\"\n")
@@ -66,8 +67,6 @@ class ContestController @Inject()(config: Configuration) extends Controller {
         val future = Actors.databaseActor ? RequestStatus(id)
         val result = Await.result(future, timeout.duration).asInstanceOf[ContestStatus]
         val resultJson = result.toJson.toString
-
-        println(resultJson)
         Ok(resultJson)
       }
       .getOrElse(BadRequest("\"Invalid contestId\""))
