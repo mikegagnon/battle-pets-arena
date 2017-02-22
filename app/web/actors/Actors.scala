40a746cdf70f344c74a7df8b3c944b31c59f8ad9
Michael Gagnon <mikegagnon@gmail.com>
2017-02-21 17:58:52 -0700

    Cleanup imports for app/web/actors/Actors.scala

--- app/web/actors/Actors.scala
+++ app/web/actors/Actors.scala
@@ -1,20 +1,9 @@
 package me.michaelgagnon.pets.web.actors
 
-import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
-import akka.event.Logging
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.model.headers._
-import akka.stream.ActorMaterializer
-import akka.stream.ActorMaterializerSettings
 import java.util.UUID
-import scala.collection.mutable.{Map => MutableMap}
-import scala.concurrent._
-import scala.util.{Success, Failure}
 
-import me.michaelgagnon.pets.web.ContestResultWithId
 import me.michaelgagnon.pets.web.controllers.ContestRequest
 
 case class ContestWithId(contest: ContestRequest, contestId: UUID)
