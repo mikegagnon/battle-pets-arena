68d03e555cebe81ff797ceea9e30294288e27115
Michael Gagnon <mikegagnon@gmail.com>
2017-02-21 18:02:25 -0700

    Partial import cleanup for app/web/actors/DatabaseActor.scala

--- app/web/actors/DatabaseActor.scala
+++ app/web/actors/DatabaseActor.scala
@@ -1,22 +1,22 @@
 package me.michaelgagnon.pets.web.actors
 
 import akka.actor.Actor
-import akka.actor.ActorSystem
-import akka.actor.Props
+//import akka.actor.ActorSystem
+//import akka.actor.Props
 import akka.event.Logging
-import akka.http.scaladsl.Http
+//import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers._
-import akka.stream.ActorMaterializer
-import akka.stream.ActorMaterializerSettings
+//import akka.stream.ActorMaterializer
+//import akka.stream.ActorMaterializerSettings
 import java.util.UUID
 import scala.collection.mutable.{Map => MutableMap}
 import scala.concurrent._
-import scala.util.{Success, Failure}
+//import scala.util.{Success, Failure}
 
-import me.michaelgagnon.pets.contest.ContestResult
-import me.michaelgagnon.pets.contest.Games
-import me.michaelgagnon.pets.web.controllers.ContestRequest
+//import me.michaelgagnon.pets.contest.ContestResult
+//import me.michaelgagnon.pets.contest.Games
+// import me.michaelgagnon.pets.web.controllers.ContestRequest
 import me.michaelgagnon.pets.web.{ContestStatus, NoStatus}
 
 import play.api.libs.json._
